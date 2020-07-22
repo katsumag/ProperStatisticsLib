b8044766b1fd17bfff870a7b9d884884fc3a3bc9
katsumag <goude47@gmail.com>
2020-07-22 07:08:44 +0100

    update to satisfy BM

--- src/main/kotlin/me/katsumag/properstatisticslib/ProperStatistic.kt
+++ src/main/kotlin/me/katsumag/properstatisticslib/ProperStatistic.kt
@@ -2,24 +2,16 @@ package me.katsumag.properstatisticslib
 
 import org.bukkit.plugin.java.JavaPlugin
 
-class ProperStatistic(private val type: Any, private val listener: StatisticListener, private val plugin: JavaPlugin) {
+class ProperStatistic(val type: Any, val listener: StatisticListener, val plugin: JavaPlugin) {
 
     init {
         StatisticsManager.registerStatistic(this)
     }
 
-    private var amount = 0
-
-    fun getAmount() = amount
+    var amount = 0
 
     fun decrement() = amount--
 
     fun increment() = amount++
-
-    fun getProvider() = listener
-
-    fun getType() = type
-
-    fun getPlugin() = plugin
 
 }
